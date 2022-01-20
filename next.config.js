361e656047c3df666ff279e1d9ca019334b9fe60
SuyeonSun <suninisy@gmail.com>
2022-01-20 10:11:42 +0900

    final commit before deployment

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+// module.exports = {
+//   reactStrictMode: true,
+// }
+
 module.exports = {
-  reactStrictMode: true,
+  poweredByHeader: process.env.NODE_ENV === 'development',
+  reactStrictMode: process.env.NODE_ENV === 'development',
 }
